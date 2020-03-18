941759763c4306549c5f03860ddeb62412eb6297
anthony mero <anthony@gladys.io>
2020-03-18 16:41:03 +0100

    - Create user module - Create user entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ReflectMetadata } from '@nestjs/common';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(),
+    UserModule,
   ],
   controllers: [AppController],
   providers: [AppService],
