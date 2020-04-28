7a1ff8105d7a14708a6f2190d3d12161fb261ec9
anthony mero <anthony@gladys.io>
2020-04-28 16:55:13 +0200

    - set physical data service methods

--- src/physical-data/physical-data.module.ts
+++ src/physical-data/physical-data.module.ts
@@ -1,4 +1,14 @@
 import { Module } from '@nestjs/common';
+import { PhysicalDataService } from './physical-data.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PhysicalData } from './physical-data.entity';
+import { User } from 'user/user.entity';
+import { UserService } from 'user/user.service';
 
-@Module({})
+@Module({
+  imports: [
+    TypeOrmModule.forFeature([PhysicalData, User]),
+  ],
+  providers: [PhysicalDataService, UserService],
+})
 export class PhysicalDataModule {}
